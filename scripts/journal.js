60c5305290d4c890e33d2c5030a13753d9d6520f
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-07-14 17:26:53 -0500

    First Daily Journal

---
+++ scripts/journal.js
@@ -0,0 +1,26 @@
+const allJournalEntries = [];
+
+const journalEntry1 = {
+  date: "",
+  concepts: "",
+  entry: "",
+  mood: ""
+}
+
+const journalEntry2 = {
+  date: "",
+  concepts: "",
+  entry: "",
+  mood: ""
+}
+
+const journalEntry3 = {
+  date: "",
+  concepts: "",
+  entry: "",
+  mood: ""
+}
+
+allJournalEntries.push(journalEntry1, journalEntry2, journalEntry3);
+
+console.log(allJournalEntries);
